5b5234872f9447c8c2b363c5f12823bb0bf4297d
Miguel Angel Morales <miguel.moralesr@hotmail.com>
2021-03-06 19:41:10 -0600

    index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const {join} = require("path");
 
 //     return res.sendfile(join(__dirname,'./dist/index.html'))
 // })
-app.use(express.static(join(__dirname, "/dist/")));
+app.use(express.static(join(__dirname, "/dist/pwa/")));
 app.get("*", (req, res) => {
   console.log(join(__dirname, "/dist/pwa/index.html"));
 
