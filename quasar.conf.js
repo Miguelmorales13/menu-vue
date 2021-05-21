d5a62069d5af7ea5dcadf159ca9b4a07e8339a61
miguel <miguel.moralesr@hotmail.com>
2021-05-21 13:36:12 -0500

    c

--- quasar.conf.js
+++ quasar.conf.js
@@ -8,7 +8,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const {configure} = require('quasar/wrappers');
 
-module.exports = configure(function (/* ctx */) {
+module.exports = configure(function (ctx ) {
   return {
     // https://quasar.dev/quasar-cli/supporting-ts
     supportTS: true,
@@ -47,8 +47,8 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'history', // available values: 'hash', 'history'
+      appBase: ctx.mode.build? '/menu-vue/':'/',
       // transpile: false,
-
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
       // Applies only if "transpile" is set to true.
